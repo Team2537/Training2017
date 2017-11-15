99e10184920674506b18b33468c6507b21ba4911
Space RAIDers <Space RAIDers@Antimatter>
2017-11-14 19:27:26 -0500

    Put in Sensors code and introduced ports

--- Training2017/src/org/usfirst/frc/team2537/robot/Ports.java
+++ Training2017/src/org/usfirst/frc/team2537/robot/Ports.java
@@ -1,5 +1,15 @@
 package org.usfirst.frc.team2537.robot;
 
 public class Ports {
+	//This is Where All of our ports go 
+	
+	//Talons
+	public static final int LEFT_TALON = 1, RIGHT_TALON = 2;
+	
+	//ULTRASONIC
+	public static final int ULTRAONIC_TRIGGER = 9, ULTRASONIC_ECHO = 8;
+	
+	//Digital Inputs
+	public static final int LIMITSWITCH_ONE = 3;
 
 }
--- Training2017/src/org/usfirst/frc/team2537/robot/Robot.java
+++ Training2017/src/org/usfirst/frc/team2537/robot/Robot.java
@@ -1,11 +1,34 @@
 package org.usfirst.frc.team2537.robot;
 
+import com.ctre.CANTalon;
+
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Ultrasonic;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
 
+	final String defaultAuto = "Default";
+	final String customAuto = "My Auto";
+	String autoSelected;
+	SendableChooser<String> chooser = new SendableChooser<>();
+	CANTalon leftTalon;
+	CANTalon rightTalon;
+	Ultrasonic ultron;
+	DigitalInput limitSwitch;
+	
 	@Override
 	public void robotInit() {
+		chooser.addDefault("Default Auto", defaultAuto);
+		chooser.addObject("My Auto", customAuto);
+		SmartDashboard.putData("Auto choices", chooser);
+		leftTalon = new CANTalon(Ports.LEFT_TALON);
+		rightTalon = new CANTalon(Ports.RIGHT_TALON);
+		ultron = new Ultrasonic(Ports.ULTRAONIC_TRIGGER, Ports.ULTRASONIC_ECHO);
+		limitSwitch = new DigitalInput(Ports.LIMITSWITCH_ONE);
+		ultron.setAutomaticMode(true);
 
 	}
 
@@ -19,6 +42,16 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void autonomousPeriodic() {
+		
+		double range = ultron.getRangeMM();
+		boolean limitEngaged = limitSwitch.get();
+		leftTalon.set(-.2);
+		rightTalon.set(.2);
+		// if (limitEngaged)
+		if (range/25.3 <= 10){
+			leftTalon.set(0);
+			rightTalon.set(0);
+		}
 
 	}
 
