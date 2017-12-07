14da70c46cf7b0e708fb41422a3f8529071cf22a
SpaceRaiders <SpaceRaiders@SpaceRaiders-PC>
2017-12-06 19:48:14 -0500

    Cire and Toby's code

---
+++ Training2017/src/org/usfirst/frc/team2537/robot/DriveSubsystem.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team2537.robot;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class DriveSubsystem extends Subsystem {
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
