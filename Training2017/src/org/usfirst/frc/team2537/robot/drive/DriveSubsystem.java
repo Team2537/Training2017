1b274a839334bc0783fcffa65356acf226982d2c
Space RAIDers <Space RAIDers@Antimatter>
2017-12-13 20:14:02 -0500

    basic stuff

--- Training2017/src/org/usfirst/frc/team2537/robot/drive/DriveSubsystem.java
+++ Training2017/src/org/usfirst/frc/team2537/robot/drive/DriveSubsystem.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team2537.robot.drive;
 
+import org.usfirst.frc.team2537.robot.Ports;
+
 import com.ctre.CANTalon;
 
 import edu.wpi.first.wpilibj.DigitalInput;
@@ -12,9 +14,20 @@ public class DriveSubsystem extends Subsystem {
 	Ultrasonic ultron;
 	DigitalInput limitSwitch;
 
+	public DriveSubsystem(){
+		leftTalon = new CANTalon(Ports.LEFT_TALON);
+		rightTalon = new CANTalon(Ports.RIGHT_TALON);
+		ultron = new Ultrasonic(Ports.ULTRAONIC_TRIGGER,Ports.ULTRASONIC_ECHO);
+		limitSwitch = new DigitalInput(Ports.LIMITSWITCH_ONE);
+		
+	}
 	@Override
 	protected void initDefaultCommand() {
 		this.setDefaultCommand(new DriveCommand());
 	}
 
+	public void doThePath(){
+		leftTalon.setI(0.25); 
+		rightTalon.setI(0.25);
+	}
 }
